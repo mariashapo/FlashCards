a03800c1e50c34a8bc26df8070db8da4b4e8dba9
Vadim Dunaevskiy <vadimdunaevskiy@dyn3138-152.wlan.ic.ac.uk>
2023-11-29 12:01:22 +0000

    established a connection to supabase

--- api/app.py
+++ api/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request, redirect
 import requests
+from supabase import create_client, Client
+
+# Supabase credentials
+SUPABASE_URL = 'https://qfgwfjebnbvfijeaejza.supabase.co'
+SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmZ3dmamVibmJ2ZmlqZWFlanphIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMTI1MzQyOSwiZXhwIjoyMDE2ODI5NDI5fQ.8EnvGn8JINHB5gEu0hWTKvsC7AqSDGPrH12bTBjIMT4'
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
 
 app = Flask(__name__)
 
@@ -11,6 +18,9 @@ def index():
 def add_word():
     word = request.form.get("word")
     translation = request.form.get("translation")
+     # Insert data into Supabase
+    data = {"word1": word, "word2": translation}
+    response = supabase.table("Flashcards").insert(data).execute()
     print(word)
     print(translation)
     return render_template("display_words.html")
